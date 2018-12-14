ba1fcb7f1a31b5f00ee7960082b360d521da5dad
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2018-12-14 10:22:35 -0500

    depend on forked lmc as opposed to GoldLab/lmc

--- MATLAB_scripts/explore_data_file.m
+++ MATLAB_scripts/explore_data_file.m
@@ -1,5 +1,7 @@
 % read pilot1.mat file to find out whether it contains the dots stimulus
 %full path: /Users/adrian/SingleCP_DotsReversal/topsDataLog/pilot1.mat
-inFilename = 'pilot1.mat';
+inFilename = 'pilot2.mat';
 taskName = 'SingleCP_DotsReversal';
-[topNode, FIRA] = topsTreeNodeTopNode.getDataFromFile(inFilename, taskName);
+fileWithPath = ['/Users/adrian/', taskName, '/topsDataLog/', inFilename];
+contents = who('-file',fileWithPath);
+[topNode, FIRA] = topsTreeNodeTopNode.getDataFromFileWithoutWho(inFilename, taskName, contents);
