bfba96c31854c7a0986216f143fe222dc558e739
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2019-01-28 10:55:35 -0500

    add code to read topsDataLog's frameInfo group from .mat file

--- MATLAB_scripts/explore_data_file.m
+++ MATLAB_scripts/explore_data_file.m
@@ -1,7 +1,15 @@
 % read pilot1.mat file to find out whether it contains the dots stimulus
 %full path: /Users/adrian/SingleCP_DotsReversal/topsDataLog/pilot1.mat
-inFilename = 'data_2019_01_14_10_21.mat';
+clear
+inFilename = 'data_2019_01_28_10_22.mat';
 taskName = 'SingleCP_DotsReversal';
-fileWithPath = ['/Users/adrian/', taskName, '/topsDataLog/', inFilename];
+
 %contents = who('-file',fileWithPath);
 [topNode, FIRA] = topsTreeNodeTopNode.getDataFromFile(inFilename, taskName);
+
+% the lines below illustrate how to access a specific group in the
+% topsDataLog from the .mat file
+fileWithPath = ['/Users/adrian/',taskName,'/topsDataLog/',inFilename];
+The_Data_Log = topsDataLog.theDataLog(true);
+topsDataLog.readDataFile(fileWithPath);
+all_frameInfo = The_Data_Log.getAllItemsFromGroup('frameInfo');
