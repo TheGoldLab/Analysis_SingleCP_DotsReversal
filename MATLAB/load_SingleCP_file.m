71873e7e1ec9074610d335819f29f3d43f337571
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2019-07-23 15:11:46 -0400

    try to fix previous bug
    === bug description ===
    
    when calling load_single_file.m on session 2 from subject 1, an error was thrown at
    the time of concatenating the FIRA table, and the error message said that the trialStart
    variable was a cell in one table and a non-cell in the other table.
    I pinpointed that this error occurred at the time of trying to concatenate data from Block2.
    
    === bug presumed cause ===
    
    the trialStart and trialEnd fields of the trialData struct from the Quest block were empty vectors
    ([]) for many trials. Because of this, calling the function struct2table would result in a column
    having the type of a cell.
    
    === bug fix ===
    
    before calling struct2table, I replace []-values by nan.

--- MATLAB/load_SingleCP_file.m
+++ MATLAB/load_SingleCP_file.m
@@ -1,4 +1,7 @@
 function [topNode, FIRA] = load_SingleCP_file(pathname, filename)
+% Args:
+%    pathname - path ending with '/' where file dumped by task resides
+%    filename - actual filename that was dumped by the MATLAB task
 
 % Clear the data log
 topsDataLog.theDataLog(true);
@@ -22,8 +25,18 @@ for c = 1:numChildren
         continue
     else
         disp('in the loop')
+        disp(task.name)
+        
+        % replace empty vector entries by NaN before merging into table
+        for trial=1:length(trials)
+            if isempty(trials(trial).trialStart)
+                trials(trial).trialStart = nan;
+            end
+            if isempty(trials(trial).trialEnd)
+                trials(trial).trialEnd = nan;
+            end            
+        end
         FIRA = [FIRA; struct2table(trials)];
-	disp(task.name)
     end
 end
 disp('exiting load_SingleCP_file')
