ba1e6c114927bf3743d2ef8fe122f97ecbaebdb9
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2019-07-25 15:59:33 -0400

    prepare for dump8 with customFIRA

--- MATLAB/customFIRA.m
+++ MATLAB/customFIRA.m
@@ -1,7 +1,7 @@
 %% test section
 tbUseProject('Analysis_SingleCP_DotsReversal');
-filename = '2019_06_20_13_45_topsDataLog.mat';
-pathname = '/Users/adrian/SingleCP_DotsReversal/raw/2019_06_20_13_45/';
+filename = '2019_06_21_13_34_topsDataLog.mat';
+pathname = '/Users/adrian/SingleCP_DotsReversal/raw/2019_06_21_13_34/';
 D = topsDataLog.readDataFile([pathname,filename]);
 LD = length(D);
 trial_groups = {};  % one group per block in the task
@@ -29,4 +29,4 @@ for i = 1:LD
         old_taskID = new_taskID;
     end
 end
-save(['trials_', filename], 'trial_groups', '-v7.3')
+save([pathname, 'trials_', filename], 'trial_groups', '-v7.3')
