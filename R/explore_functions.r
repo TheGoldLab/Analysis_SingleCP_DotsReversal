45240512d873430864b5b6b233f43f5c1ffb6260
Adrian Radillo <adrian.radillo@gmail.com>
2019-09-02 14:49:07 -0400

    produce theoretical accuracy plots separately

--- R/explore_functions.r
+++ R/explore_functions.r
@@ -31,4 +31,50 @@ get_full_data <- function() {
   return(data)
 }
 
+#####
+# Theoretical accuracy functions
+####
 
+# accuracy of perfect accumulator on non-CP trials
+acc <- function(C, t) {
+  return(pnorm(C*sqrt(t)))
+}
+
+# accuracy of perfect accumulator on CP trials
+# C represents |d|/sigma with d the drift rate and sigma the diffusion constant
+acc_cp <- function(C, t) {
+  if (t<=.2) {
+    return(pnorm(C*sqrt(t)))
+  } else {
+    ccc <- t - .4  # in my equations this is -(T-tau) 
+    return(pnorm(C*ccc/sqrt(t)))
+  }
+}
+
+
+
+# theoretical accuracy of a linear-leak model (O-U with drift)
+# C is |drift|/diffusion , t is time in sec, leak is leak rate, cp is bool: whether there is a cp or not
+acc_leak <- function(C, t, leak, cp) {
+  if (cp) {
+    if (t <= .2) {  # before CP, compute as for cp=F case
+      return(
+        pnorm(
+          C * ( 1 - exp(-leak*t) ) /  sqrt( (leak / 2) * (1 - exp(-2*leak*t)) )
+        )
+      )
+    } else {  # after CP
+      tau <- t-.2
+      return(pnorm(
+        C * (1-exp( -leak * tau ) * (2-exp( -leak*0.2 ))) / 
+          sqrt( (leak / 2) * (1 - exp(-2*leak*t)) )
+      ))
+    }
+  } else {
+    return(
+      pnorm(
+        C * ( 1 - exp(-leak*t) ) /  sqrt( (leak / 2) * (1 - exp(-2*leak*t)) )
+      )
+    )
+  }
+}
--- R/plot_functions.r
+++ R/plot_functions.r
@@ -3,8 +3,16 @@ library(data.table)
 library(ggforce)
 source("explore_functions.r")
 
+############# SET UP DATA.TABLES NEEDED
+
 data <- get_full_data() 
 nonQuestData <- data[block != "Quest"]
+factored_threshold <- nonQuestData[,coh_cat:="0"]
+factored_threshold[coherence > 0, coh_cat:= "th"]
+factored_threshold[coherence==100, coh_cat:="100"]
+factored_threshold[,coh_cat:=factor(coh_cat, levels=c("0", "th", "100"), ordered=T)]
+
+#############
 
 ## Acc (DD) on non-CP trials as function of VD for threshold coherence, by subject by probCP
 #to_plot2 <- nonQuestData[
@@ -33,10 +41,6 @@ nonQuestData <- data[block != "Quest"]
 
 #Accuracy (DD) on non-CP trials as a function of coherence, for fixed viewing duration.
 
-factored_threshold <- nonQuestData[,coh_cat:="0"]
-factored_threshold[coherence > 0, coh_cat:= "th"]
-factored_threshold[coherence==100, coh_cat:="100"]
-factored_threshold[,coh_cat:=factor(coh_cat, levels=c("0", "th", "100"), ordered=T)]
 
 #to_plot3 <- factored_threshold[
 #    presenceCP == "no-CP",
@@ -151,31 +155,119 @@ factored_threshold[,coh_cat:=factor(coh_cat, levels=c("0", "th", "100"), ordered
 
 
 
-#The probability of being correct on 300-msec change-point trials
-#should be greater (but still below %50) than the probability of being
-#wrong on 100-msec trials
-to_plota <- factored_threshold[
-  ((viewingDuration == 100) | (viewingDuration == 300 & presenceCP == "CP")) &
-    coh_cat=="th",
-  .(accuracy=mean(dirCorrect), numTrials=.N),
-  by=.(presenceCP, subject, viewingDuration, probCP)
-]
-to_plota[, y:=accuracy]
-to_plota[viewingDuration==100, y:=1-y]  # P(wrong)for 100 msec
-to_plota[,se:=sqrt(y * (1-y) / numTrials)]
-to_plota[,ci:=1.96*se]
+##The probability of being correct on 300-msec change-point trials
+##should be greater (but still below %50) than the probability of being
+##wrong on 100-msec trials
+#to_plota <- factored_threshold[
+#  ((viewingDuration == 100) | (viewingDuration == 300 & presenceCP == "CP")) &
+#    coh_cat=="th",
+#  .(accuracy=mean(dirCorrect), numTrials=.N),
+#  by=.(presenceCP, subject, viewingDuration, probCP)
+#]
+#to_plota[, y:=accuracy]
+#to_plota[viewingDuration==100, y:=1-y]  # P(wrong)for 100 msec
+#to_plota[,se:=sqrt(y * (1-y) / numTrials)]
+#to_plota[,ci:=1.96*se]
+#
+#
+#png(filename="pwrong_pcorr_100-300_bysubj_bypcp_bycp.png", width=1150, height=1300)
+#ggplot(to_plota, aes(x=factor(viewingDuration), y=y, col=presenceCP)) +
+#  geom_point(size=5) +
+#  geom_hline(yintercept=c(0, .5), color="black", linetype="dashed") +
+#  geom_errorbar(aes(ymin=y-ci, ymax=y+ci), width=.1, size=2) +
+#  facet_grid(subject~probCP) +
+#  labs(title="") +
+#  xlab("Viewing Duration") + ylab("P(wrong) vs. P(correct)") +
+#  ggtitle("P(wrong|noCP) vs. P(correct|CP) \n on 100 vs. 300 CP trials") +
+#  scale_color_manual(labels = c("P(wrong|noCP)", "P(correct|CP)"), values = c("darkred", "darkgreen")) + 
+#  theme(text = element_text(size=35))  
+##  theme_bw()
+#dev.off()
 
 
-png(filename="pwrong_pcorr_100-300_bysubj_bypcp_bycp.png", width=1150, height=1300)
-ggplot(to_plota, aes(x=factor(viewingDuration), y=y, col=presenceCP)) +
-  geom_point(size=5) +
-  geom_hline(yintercept=c(0, .5), color="black", linetype="dashed") +
-  geom_errorbar(aes(ymin=y-ci, ymax=y+ci), width=.1, size=2) +
-  facet_grid(subject~probCP) +
-  labs(title="") +
-  xlab("Viewing Duration") + ylab("P(wrong) vs. P(correct)") +
-  ggtitle("P(wrong|noCP) vs. P(correct|CP) \n on 100 vs. 300 CP trials") +
-  scale_color_manual(labels = c("P(wrong|noCP)", "P(correct|CP)"), values = c("darkred", "darkgreen")) + 
-  theme(text = element_text(size=35))  
-#  theme_bw()
+
+
+############ THEORETICAL ACCURACY SECTION ################
+
+baseC <- seq(.2, 4, .1)
+baset <- seq(.1, .4, .01)
+
+C <- c()
+t <- c()
+norm_ncp <- c()
+norm_cp <- c()
+lowleak_ncp <- c()
+highleak_ncp <- c()
+lowleak_cp <- c()
+highleak_cp <- c()
+
+#actual leak values
+lowl <- 3
+highl <- 10
+
+# iteration <- 1
+
+for (cc in baseC) {
+  for (tt in baset) {
+    C <- c(C, cc)
+    t <- c(t, tt)
+    norm_ncp <- c(norm_ncp, acc(cc, tt))
+    norm_cp <- c(norm_cp, acc_cp(cc, tt))
+    lowleak_ncp <- c(lowleak_ncp, acc_leak(cc, tt, lowl, F))
+    highleak_ncp <- c(highleak_ncp, acc_leak(cc, tt, highl, F))
+    lowleak_cp <- c(lowleak_cp, acc_leak(cc, tt, lowl, T))
+    highleak_cp <- c(highleak_cp, acc_leak(cc, tt, highl, T))
+    # print(iteration)
+    # print(norm_ncp)
+    # iteration <- iteration + 1
+  }
+}
+
+data <- data.table(C, t, norm_ncp, norm_cp, lowleak_ncp, highleak_ncp, lowleak_cp, highleak_cp)
+
+#convert to long format
+long_data = melt(data, id.vars = c("C", "t"),
+                measure.vars = c("norm_ncp", "norm_cp", "lowleak_ncp", "highleak_ncp", "lowleak_cp", "highleak_cp"))
+long_data[,Pwrong := 1-value]
+
+long_data[,CP:="no-CP"]
+long_data[,model:="DDM"]
+
+long_data[variable == "norm_cp" |
+            variable == "lowleak_cp" |
+            variable == "highleak_cp", `:=`(CP="CP")]
+long_data[variable == "lowleak_cp" | variable == "lowleak_ncp", `:=`(model="Low Leak")]
+long_data[variable == "highleak_cp" | variable == "highleak_ncp", `:=`(model="High Leak")]
+long_data[,model:=factor(model, levels = c("DDM", "Low Leak", "High Leak"))]
+long_data[,CP:=factor(CP, levels=c("no-CP", "CP"))]
+
+##### Heat maps
+
+png(filename="heatmaps_theoretical_acc.png", width=900, height=1000)
+ggplot(data=long_data, aes(x=t, y=C, fill=value, group=variable)) + 
+      theme_bw() +
+      geom_tile() +
+      ggtitle("Theoretical Accuracy") + 
+      scale_fill_gradientn(colors=colorRampPalette(c("white","royalblue","seagreen","orange","red","brown"))(500),name="Accuracy\n[P(correct)]") +
+      labs(x = "Time [sec]",y="SNR [|d|/sigma]") +
+      facet_grid(model~CP) + 
+      theme(text=element_text(size=32)) 
 dev.off()
+
+##### Single curves
+
+png(filename="theoretical_acc_curves.png", width=1350, height=600)
+ggplot(aes(x=t, y=value, col=CP), data=long_data[C==0.8 | C==1.5 | abs(C-3)<0.0001,]) + 
+  geom_line(aes(group=interaction(model, CP), linetype=model), size=1.5) +
+  # geom_hline(yintercept = long_data[model == "DDM" & (C==0.8 | C==1.5 | abs(C-3)<0.0001) & t ==.1, Pwrong],
+  #            linetype="dotted") +
+  facet_wrap(~factor(C)) +
+  ylab("P(correct)") + xlab("time (s)") +
+  labs(title = "Theoretical Curves Perfect vs. Leaky accumulators",
+       subtitle = "By SNR", 
+       caption = paste("low leak =",lowl,"; high leak =",highl),
+       col="") +
+  theme(text=element_text(size=35), legend.key.width = unit(5, "line"))  # text size would look better at 32... 
+dev.off()
+
+#################################################
