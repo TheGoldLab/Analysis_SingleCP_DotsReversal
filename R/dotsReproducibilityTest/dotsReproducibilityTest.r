2b4fe4edcb254b859ec0a93f7aa619661c5bb5d2
Adrian Radillo <adrian.radillo@gmail.com>
2019-10-03 14:08:01 -0400

    produce dots plots for 2019_10_01_12_06

--- R/dotsReproducibilityTest/dotsReproducibilityTest.r
+++ R/dotsReproducibilityTest/dotsReproducibilityTest.r
@@ -2,10 +2,10 @@
 library(data.table)
 library(ggforce)
 
-path_to_fira <- "/home/adrian/SingleCP_DotsReversal/DotsReproducibilityTest/raw/2019_10_01_11_53/2019_10_01_11_53_FIRA.csv"
+path_to_fira <- "/home/adrian/SingleCP_DotsReversal/DotsReproducibilityTest/raw/2019_10_01_12_06/2019_10_01_12_06_FIRA.csv"
 fira <- fread(path_to_fira)
 
-path_to_dots <- "/home/adrian/SingleCP_DotsReversal/DotsReproducibilityTest/raw/2019_10_01_11_53/2019_10_01_11_53_dotsPositions.csv"
+path_to_dots <- "/home/adrian/SingleCP_DotsReversal/DotsReproducibilityTest/raw/2019_10_01_12_06/2019_10_01_12_06_dotsPositions.csv"
 dots <- fread(path_to_dots)
 
 dots[,trial:=factor(seqDumpTime, ordered=T)]
@@ -20,7 +20,7 @@ frameCounts <- frames[,.(maxFrames=max(frameIdx), numFrames=.N), by=trial]
 
 frameCounts[,reltrial:=.I]
 
-png(filename="dotsReproducibilityTest_5th_1_frameCount.png", width=400, height=350)
+png(filename="dotsReproducibilityTest_5th_2_frameCount.png", width=400, height=350)
 ggplot(frameCounts, aes(x=reltrial, y=numFrames)) +
   geom_point(size=4) +
   scale_y_continuous(breaks = seq(0, 15, 1)) +
@@ -33,13 +33,13 @@ dev.off()
 dots_dt <- dots[isActive == 1, .(xpos, ypos, trial, frameIdx)]
 dots_dt[,dotID:=seq(.N), by=.(trial, frameIdx)]
 
-png(filename="dotsReproducibilityTest_5th_1_2.png", width=5000, height=3000)
+png(filename="dotsReproducibilityTest_5th_2_2.png", width=5000, height=3000)
 ggplot(dots_dt, aes(x=xpos, y=ypos, color=xpos)) + geom_point() + facet_grid(trial~frameIdx) + theme(text=element_text(size=20))
 dev.off()
 
 dots_melted <- melt(dots_dt, measure.vars=c("xpos", "ypos"), variable.name="axis", value.name="position", id.vars=c("trial", "frameIdx", "dotID"))
 
-png(filename="dotsReproducibilityTest_5th_1_histograms_xpos_ypos.png", width=400, height=1200)
+png(filename="dotsReproducibilityTest_5th_2_histograms_xpos_ypos.png", width=400, height=1200)
 ggplot(dots_melted, aes(x=position, group=axis)) + geom_histogram(bins=500) + facet_grid(frameIdx~axis) + 
 	theme(text=element_text(size=15), panel.spacing.y=unit(0, "lines"))
 dev.off()
